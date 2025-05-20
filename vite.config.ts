48d27aa032ca129e7e18f6b5ac6b28fec44f7614
Kostin Pavel <50001467+KostinP@users.noreply.github.com>
2025-05-20 04:53:56 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,4 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/open-campus/',
-  esbuild: {
-    // Игнорировать ошибки TypeScript во время сборки
-    legalComments: "none",
-    drop: ["console", "debugger"],
-  },
 })
